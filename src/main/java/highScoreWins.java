325a2530acc7103721324192badf7f29dfa554f7
marbin701 <marvin07012002@gmail.com>
2026-04-17 17:23:35 -0400

    hihg score wins

--- src/main/java/highScoreWins.java
+++ src/main/java/highScoreWins.java
@@ -1,4 +1,23 @@
-package PACKAGE_NAME;
-
+import java.sql.SQLOutput;
+import java.util.Scanner;
 public class highScoreWins {
+    public static void main(String[] args) {
+
+
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        String[] parts = input.split("\\|");
+
+        String[] teams = parts[0].split(":");
+        String[] scores = parts[1].split(":");
+
+        int homeScore = Integer.parseInt(scores[0]);
+        int visitorScore = Integer.parseInt(scores[1]);
+
+        if (homeScore > visitorScore) {
+            System.out.println("Winner: " + teams[0]);
+        }else {
+            System.out.println("Winner: " + teams[1]);
+        }
+    }
 }
